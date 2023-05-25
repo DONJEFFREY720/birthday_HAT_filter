173abb729d91f302c51c4d40e23c2823803069d4
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-05-25 20:36:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+     hat = loadImage("https://i.postimg.cc/8cND3RGS/birthday-hat.png")
+}
+
+noseX = 0
+noseY = 0
+
+function setup(){
+     canvas = createCanvas(300,300)
+     canvas.center()
+     video = createCapture(VIDEO)
+     video.hide()
+     posenet = ml5.poseNet(video,modeLoaded)
+     posenet.on('pose',gotResults)
+}
+
+function draw(){
+     image(video,0,0,300,300)
+     image(hat,noseX,noseY-100,30,30)
+}
+
+function modeLoaded(){
+     console.log("model is ready")
+}
+
+function gotResults(results){
+     if(results.length > 0 ){
+          noseX = results[0].pose.nose.x
+          noseY = results[0].pose.nose.y
+          console.log("nose 'X' =   " +results[0].pose.nose.x)
+          console.log("nose 'Y' =   " +results[0].pose.nose.y)
+     }
+}
